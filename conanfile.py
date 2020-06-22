f419df0be4aebd53ab42fb70741a18e04800deb1
CrackTheCode016 <ibnbassem@gmail.com>
2020-06-22 19:21:42 -0400

    updated with NEM rocksdb

--- conanfile.py
+++ conanfile.py
@@ -1,30 +1,31 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools, CMake
 import os
 
 # https://github.com/giorgioazzinnaro/rocksdb/blob/master/INSTALL.md
 
+
 class RocksdbConan(ConanFile):
     name = "RocksDB"
-    version = "5.8"
-    license = "https://github.com/facebook/rocksdb/blob/master/COPYING"
+    version = "6.6.4-nem"
+    license = "https://github.com/nemtech/rocksdb/blob/master/COPYING"
     url = "https://github.com/giorgioazzinnaro/conan-RocksDB"
     description = "A library that provides an embeddable, persistent key-value store for fast storage."
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    options = {"shared": [True]}
+    default_options = {'shared': 'True'}
     generators = "cmake"
 
-    source_tgz = "https://github.com/facebook/rocksdb/archive/v%s.tar.gz" % version
+    source_tgz = "https://github.com/nemtech/rocksdb/archive/v%s.tar.gz" % version
 
     requires = (
         "zlib/1.2.11@conan/stable",
         "bzip2/1.0.6@conan/stable",
-        "LZ4/1.8.0@bincrafters/stable"
+        "lz4/1.8.0@bincrafters/stable"
         # TODO snappy, zstandard
     )
 
     def source(self):
-        self.output.info("Downloading %s" %self.source_tgz)
+        self.output.info("Downloading %s" % self.source_tgz)
         tools.download(self.source_tgz, "rocksdb.tar.gz")
 
         tools.unzip("rocksdb.tar.gz")
@@ -48,7 +49,6 @@ class RocksdbConan(ConanFile):
         with tools.chdir(self.subfolder):
             env_build = AutoToolsBuildEnvironment(self)
             env_build.fpic = True
-
             if self.settings.build_type == "Debug":
                 env_build.make()  # $ make
             else:
@@ -64,9 +64,13 @@ class RocksdbConan(ConanFile):
         self.copy("*.h", dst="include", src=("%s/include" % self.subfolder))
 
         if self.options.shared:
-            self.copy("librocksdb.so", dst="lib", src=self.subfolder, keep_path=False)
+            self.copy("librocksdb.so", dst="lib",
+                      src=self.subfolder, keep_path=False)
+            self.copy("librocksdb.dylib", dst="lib",
+                      src=self.subfolder, keep_path=False)
         else:
-            self.copy("librocksdb.a", dst="lib", src=self.subfolder, keep_path=False)
+            self.copy("librocksdb.a", dst="lib",
+                      src=self.subfolder, keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["rocksdb"]
